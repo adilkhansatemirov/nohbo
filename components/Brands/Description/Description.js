e8b66f11c44b473cd5484dd043c26c27f82b5175
Azharyer <88300303+Azharyer@users.noreply.github.com>
2021-09-05 16:11:35 -0400

    Update Description.js

--- components/Brands/Description/Description.js
+++ components/Brands/Description/Description.js
@@ -28,42 +28,9 @@ function Description() {
           </ScrollAnimation>
         </div>
 
-import React from "react";
-import { UncontrolledCarousel, Row, Col } from "reactstrap";
 
-const items = [
-  {
-    src: require("assets/img/theme/img-1-1200x1000.jpg"),
-    altText: "Slide 1",
-    caption: "",
-    header: "",
-    key: "1",
-  },
-  {
-    src: require("assets/img/theme/img-2-1200x1000.jpg"),
-    altText: "Slide 2",
-    caption: "",
-    header: "",
-    key: "2",
-  },
-  {
-    src: require("assets/img/theme/img-3-1200x1000.jpg"),
-    altText: "Slide 3",
-    caption: "",
-    header: "",
-    key: "3",
-  },
-];
 
-const Example = () => (
-  <Row>
-    <Col md="8" className="mx-auto">
-      <UncontrolledCarousel items={items} />
-    </Col>
-  </Row>
-);
 
-export default Example;
 
 
 
