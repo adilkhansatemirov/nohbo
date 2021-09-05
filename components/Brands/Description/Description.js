39e4d0ec99466f0167ae8a68952100011c546f74
Azharyer <88300303+Azharyer@users.noreply.github.com>
2021-09-05 15:27:32 -0400

    Update Description.js

--- components/Brands/Description/Description.js
+++ components/Brands/Description/Description.js
@@ -19,10 +19,10 @@ function Description() {
         </h3>
         <div className={styles.sunriseImageContainer}>
           <ScrollAnimation animateIn="fadeInRight" animateOnce>
-            <a href="https://sunrisesession.com/" target="_blank" rel="noopener noreferrer">
+            
               {/* eslint-disable-next-line @next/next/no-img-element */}
             <img className={styles.sunriseImage} src="/images/sunrise.png" alt="sunrise" />
-            <a/>
+            
           </ScrollAnimation>
         </div>
       </div>
