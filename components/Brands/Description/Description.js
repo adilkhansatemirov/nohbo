590bca982997fe48c68cb15a4c59e705e520a5bd
Azharyer <88300303+Azharyer@users.noreply.github.com>
2021-09-05 15:36:30 -0400

    Update Description.js

--- components/Brands/Description/Description.js
+++ components/Brands/Description/Description.js
@@ -22,7 +22,7 @@ function Description() {
             
             <a href="https://www.sunrisesession.com" target="_blank" rel="noopener noreferrer">
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                /*<img src="/images/linkedin-blue.svg" alt="linkedin-blue" />*/
+                {/*<img src="/images/linkedin-blue.svg" alt="linkedin-blue" />*/}
               </a>
             
               {/* eslint-disable-next-line @next/next/no-img-element */}
