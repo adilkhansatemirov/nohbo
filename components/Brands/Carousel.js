37c7492c2b2f0bb340cbde7e034f8091fd40bad6
Azharyer <88300303+Azharyer@users.noreply.github.com>
2021-09-06 03:26:30 -0400

    Update Carousel.js

--- components/Brands/Carousel.js
+++ components/Brands/Carousel.js
@@ -5,16 +5,16 @@ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
+  var int;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  for (int = 0; int < slides.length; int++) {
+    slides[int].style.display = "none";  
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (int = 0; int < dots.length; int++) {
+    dots[int].className = dots[int].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
